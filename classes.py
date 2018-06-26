8b2a0400a17f2adfbc312097fbc7c4e5060d5707
Jyrsin <40595215+Jyrsin@users.noreply.github.com>
2018-06-26 12:46:58 +0300

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,125 @@
+#luokka kuormille
+class load:
+    def __init__(self,name,ID,sensorPin,relayPin,maximumCurrent,priority):
+        #kuorman nimi ja ID-numero
+        self.__name = name
+        self.__ID = ID
+
+        #sensorien ja releiden pinnit
+        self.__sensorPin = sensorPin
+        self.__relayPin = relayPin
+
+        #tämän tunnin kulutus, hetkellinen maksimivirta ja raja-arvo
+        self.__curHourEne = 0
+        self.__maximumCurrent = maximumCurrent
+        self.__threshold = 0.9
+
+        #muuttujat, jotka kertovat pitääkö releitä ohjata
+        self.__autoCont = 0
+        self.__manualCont = 0
+
+        #kertoo mikä prioriteetti kuorman pudottamisella on (päättäkää suunta)
+        self.__priority = priority
+
+    #vaihtaa pinniä, jossa rele on
+    def changeRelayPin(self,newPin):
+        self.__relayPin = newPin
+
+    #kutsutaan tunnin välein, resettaa tunnin kulutuksen
+    def resetHour(self):
+        self.__curHourEne = 0
+
+    #antaa tämänhetkisen kulutuksen
+    def getCons(self):
+        print("tämä antaa tämänhetkisen kulutuksen vaiheessa")
+
+    #antaa infoa kuormasta
+    def info(self):
+        print("Name:",self.__name)
+        print("ID:",self.__ID)
+        print("Pin of the sensor:",self.__sensorPin)
+        print("Pin of the relay:",self.__relayPin)
+        print("This hour's consumed energy:",self.__curHourEne)
+        print("Maximum current of this load:",self.__maximumCurrent)
+        print("Load's dropping priority:",self.__priority)
+
+    #antaa kuorman nimen
+    def getName(self):
+        return self.__name
+
+
+
+#luokka päävaiheille
+class mainPhase:
+    def __init__(self,name,ID,sensorPin,maximumCurrent,maxHour):
+        #päävaiheen nimi ja ID-numero
+        self.__name = name
+        self.__ID = ID
+
+        #virtasensorin pinni ja lista, jossa vaiheen kuormat
+        self.__sensorPin = sensorPin
+        self.__loads = []
+
+        #tämän tunnin kulutus, maksimi hetkellinen virta ja raja-arvo
+        self.__curHourEne = 0
+        self.__maximumCurrent = maximumCurrent
+        self.__threshold = 0.9
+
+        #tämän kuukauden suurin tehotunti ja milloin se oli ja maksimi tehotunti joka on sallittu
+        self.__curMonthMax = maxHourDate(maxHour,9,5,2018)
+        self.__maxHour = maxHour
+
+    #lisää vaiheeseen kuorman
+    def addLoad(self,load):
+        self.__loads.append(load)
+
+    #kutsutaan tunnin välein, resettaa tunnin kulutuksen
+    def resetHour(self):
+        self.__curHourEne = 0
+
+    #antaa tämänhetkisen kulutuksen
+    def getCons(self):
+        print("tämä antaa tämänhetkisen kulutuksen vaiheessa")
+
+    #antaa infoa päävaiheesta
+    def info(self):
+        print("Name:",self.__name)
+        print("ID:",self.__ID)
+        print("Pin of the sensor:",self.__sensorPin)
+        print("This hour's consumed energy:",self.__curHourEne)
+        print("Maximum current of this load:",self.__maximumCurrent)
+        print("Maximum hourly consumption:",self.__maxHour)
+        self.__curMonthMax.info()
+        print("Loads:")
+        for load in self.__loads:
+            print(load.getName())
+
+
+
+#luokka kuukauden maksimitunnille, kertoo sekä tunnin kulutuksen, että päivämäärän
+class maxHourDate:
+    def __init__(self,maxHour,day,month,year):
+        self.__maxHour = maxHour
+        self.__month = month
+        self.__year = year
+        self.__date = str(day) + "." + str(self.__month) + "." +  str(self.__year)
+
+    def info(self):
+        print("The biggest consumption in an hour this month:",self.__maxHour)
+        print("And it was in:",self.__date)
+
+
+
+#testausta
+kuorma1 = load("Lattialämmitys",12345,1,2,16,0)
+kuorma1.info()
+kuorma1.changeRelayPin(5)
+print("")
+kuorma1.info()
+
+print("")
+
+päävaihe1 = mainPhase("Päävaihe 1",98765,3,40,1600)
+päävaihe1.addLoad(kuorma1)
+päävaihe1.info()
+
