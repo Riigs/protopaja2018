da0288904b4b8952e11b320a96d2c3297697b45c
Riigs <riikkan0@gmail.com>
2018-06-28 17:10:21 +0300

    Funktio, joka mittaa jännitettä

--- sipy/lib/mittaus.py
+++ sipy/lib/mittaus.py
@@ -1,20 +1,15 @@
 #Poista kommentit pycom ja machine kun ajat sipyllä
 import pycom
-import machine #rajoita tilan säästämiseksi?
+import machine
 import time
 
-#lukee jännitettä.
-#Kun ajat ohjelmaa Sipyllä, merkkaa "#POIS" merkityt rivit kommenteiksi,
+#Palauttaa sensorin pinnin lukeman jännitteelle
 def adc_read(sensorPin):
-    #''' #POIS
     adc = machine.ADC()
     apin = adc.channel(pin=sensorPin)
     val = apin()
     time.sleep(2)
     return val
-    #''' #POIS
-    #time.sleep(2) #POIS
-    #return 1  #POIS
 
 def val_to_volt(val):
     max = 4095
