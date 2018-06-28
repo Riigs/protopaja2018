e4f939e77922e75c7968d049edce3f1d41c4a173
Jyrsin <40595215+Jyrsin@users.noreply.github.com>
2018-06-28 14:07:18 +0300

    Toteutettu tiedostosta avaaminen kuormille ja vaiheille

--- main.py
+++ main.py
@@ -2,10 +2,24 @@
 from classes import *
 
 #funktioiden määrittely
-def openLoads(loads):
+def openLoads(loads,loadFile):
+    loadData = open(loadFile,'r')
+    for line in loadData:
+        data = line.split(',')
+        if len(data) == 7:
+            newLoad = load(data[0],data[1],data[2],data[3],data[4],data[5],data[6])
+            loads.append(newLoad)
+    loadData.close()
     return
 
-def openPhases(phases):
+def openPhases(phases,phasesFile):
+    phasesData = open(phasesFile,'r')
+    for line in phasesData:
+        data = line.split(',')
+        if len(data) == 4:
+            newPhase = mainPhase(data[0],data[1],data[2],data[3])
+            phases.append(newPhase)
+    phasesData.close()
     return
 
 def openMonthMax():
@@ -22,7 +36,8 @@ def main():
         #ohjauksen tarkistaminen pilvestä
         #ohjauksen tarkistaminen automaattisesti
         #releiden tilojen muuttaminen (virran katkominen tai palauttaminen)
-        print("kraak")
+        loads[0].info()
+        phases[0].info()
         running = False
 
 #muuttujien asettaminen ja tietojen lataaminen tiedostosta, ja hard
@@ -43,11 +58,11 @@ monthMaxFile = "monthMax.txt"
 
 #avataan eri kuormat tiedostosta
 loads = []
-openLoads(loads)
+openLoads(loads,loadFile)
 
 #avataan tiedot eri vaiheista tiedostosta
 phases = []
-openPhases(phases)
+openPhases(phases,phaseFile)
 
 #avataan tiedot kuukauden suurimmasta tuntitehosta tiedostosta
 monthMax = openMonthMax()
