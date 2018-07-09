e45b4114c770e9dafaad1b78bf6d64814865bb37
Jyrsin <40595215+Jyrsin@users.noreply.github.com>
2018-07-09 15:35:26 +0300

    debuggauskorjhausta

--- sipy/lib/mittaus.py
+++ sipy/lib/mittaus.py
@@ -21,23 +21,23 @@ def adc_read(sensorPin):
 #Tallentaa lukeman kansioon temp/ID.txt
 def adc_save(val, ID):
     filename = str(ID)+".txt"
-    path = os.path.join("temp", filename)
-    with open(path, "a") as f:
+    #path = os.path.join("temp", filename)
+    path = "temp/" + filename
+    print(path)
+    #f = open(path,'a+')
+    #f.write(str(val)+"\n")
+    #f.close()
+    with open(path, "a+") as f:
         f.write(str(val)+"\n")
+
+def adcFileRead():
+    file = open("temp/12345.txt", 'r')
+    data = file.read()
+    print(data)
+    file.close()
 
 def rm(filename):
     os.remove(filename)
-
-def calc__curHourEne(filename):
-    I = 2 #lasketaan jostain?
-    sum = 3
-    with open(filename, 'r') as loadData:
-        for line in loadData:
-            num = float(line)
-            val = val_to_volt(num)
-            sum += val
-        return(sum*I)
-
 
 def val_to_volt(val):
     max = 4095
